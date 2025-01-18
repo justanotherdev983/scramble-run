c4dc3f5cea93c210702b4b34890f60e8748724f1
justanotherdev983 <106712@broklede.nl>
2025-01-18 17:05:40 +0100

    Humble beginnings

--- src/cmd/server/main.go
+++ src/cmd/server/main.go
@@ -2,15 +2,50 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
+
+type RaceInfo struct {
+	Id           int
+	Name         string
+	ChickenNames []string
+}
+
+type User struct {
+	Name string
+	Age  int
+}
+type PageData struct {
+	Title    string
+	UserData User
+	Races    RaceInfo
+}
 
 func main() {
 	const local_port string = "6969"
 
+	tmpl := template.Must(template.ParseGlob("src/web/templates/*.gohtml"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("src/web/static"))))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintln(w, "<h1> Text </h1>")
+		data := PageData{
+			Title: "Scramble Run",
+			UserData: User{
+				Name: "test_user",
+				Age:  99,
+			},
+			Races: RaceInfo{
+				Id:           1,
+				Name:         "test_race",
+				ChickenNames: []string{"test_chicken1", "test_chicken2"},
+			},
+		}
+
+		err := tmpl.ExecuteTemplate(w, "home.gohtml", data)
 		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	})
