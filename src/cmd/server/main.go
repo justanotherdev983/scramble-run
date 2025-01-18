5cbb1e7828fddc47ad4416080a968c8c22a52935
justanotherdev983 <106712@broklede.nl>
2025-01-18 16:09:12 +0100

    Adding basic file structure, whoops

--- src/cmd/server/main.go
+++ src/cmd/server/main.go
@@ -9,12 +9,12 @@ func main() {
 	const local_port string = "6969"
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintln(w, "10 euro op gele kip!")
+		_, err := fmt.Fprintln(w, "<h1> Text </h1>")
 		if err != nil {
 			return
 		}
 	})
-	
+
 	fmt.Printf("Server started on http://localhost:%s\n", local_port)
 	err := http.ListenAndServe(":"+local_port, nil)
 	if err != nil {
